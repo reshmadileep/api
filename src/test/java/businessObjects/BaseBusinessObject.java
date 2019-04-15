80ed9b043c75d1eea8cf251aa19363dd401ef009
Navin Joseph Dhanaraj <Navin.Dhanaraj@team.telstra.com>
2019-04-15 15:06:28 +1000

    Added equals and hashcode functions

--- src/test/java/businessObjects/BaseBusinessObject.java
+++ src/test/java/businessObjects/BaseBusinessObject.java
@@ -1,5 +1,28 @@
 package businessObjects;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 public class BaseBusinessObject {
     public String id = null;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (!(o instanceof BaseBusinessObject)) return false;
+
+        BaseBusinessObject that = (BaseBusinessObject) o;
+
+        return new EqualsBuilder()
+                .append(id, that.id)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(id)
+                .toHashCode();
+    }
 }
--- src/test/java/businessObjects/Post.java
+++ src/test/java/businessObjects/Post.java
@@ -1,5 +1,8 @@
 package businessObjects;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 public class Post extends BaseBusinessObject{
     public String title,body,userID;
 
@@ -7,5 +10,31 @@ public class Post extends BaseBusinessObject{
         title = "New Post - "+ uuid;
         body = "This is the body of this wonderful post - " + uuid;
         userID = uuid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (!(o instanceof Post)) return false;
+
+        Post post = (Post) o;
+
+        return new EqualsBuilder()
+                .appendSuper(super.equals(o))
+                .append(title, post.title)
+                .append(body, post.body)
+                .append(userID, post.userID)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .appendSuper(super.hashCode())
+                .append(title)
+                .append(body)
+                .append(userID)
+                .toHashCode();
     }
 }
